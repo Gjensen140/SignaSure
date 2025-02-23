e894e8b8306f723ed9db47189c501cb4b95e967c
Caden Wright <119685561+05cwright05@users.noreply.github.com>
2025-02-23 01:52:05 -0500

    fixes

--- SignaSure_API/signature_api/image_processor.py
+++ SignaSure_API/signature_api/image_processor.py
@@ -1,10 +1,18 @@
-import cv2
+from PIL import Image
 import numpy as np
 
 def image_cleaning(image_file, target_size=(150, 220)):
     """Converts an image to grayscale, resizes it, and returns a NumPy array"""
-    img_array = np.frombuffer(image_file.read(), np.uint8)
-    img = cv2.imdecode(img_array, cv2.IMREAD_GRAYSCALE)
-    img = cv2.resize(img, target_size, interpolation=cv2.INTER_AREA)
-    img = img.astype(np.float32) / 255.0
-    return img
+    # Convert the image to grayscale
+    img = image_file.convert("L")
+   
+    # Resize the image
+    img = img.resize(target_size, Image.LANCZOS)
+   
+    # Convert the image to a NumPy array
+    img_array = np.array(img, dtype=np.float32)
+   
+    # Normalize the pixel values to the range [0, 1]
+    img_array /= 255.0
+   
+    return img_array
