66fd8b7b859909c3f944aa5a137a53a254acee9e
Ishaan-Buddharaju <ishaan.buddharaju@gmail.com>
2025-02-23 08:47:31 -0500

    done

--- SignaSure_API/sig_api.py
+++ SignaSure_API/sig_api.py
@@ -2,7 +2,8 @@ from signature_api import create_app
 from flask_cors import CORS
 
 app = create_app()
-CORS(app)
+CORS(app, resources={r"/process_images": {"origins": "http://localhost:5173"}})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
